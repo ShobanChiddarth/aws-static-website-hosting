a4e0085c4ce52e8b0aa7a4427fb1c34c6f296e4d
Shoban Chiddarth <messageshoban@gmail.com>
2026-05-18 11:05:48 +0530

    Created sg for ec2

---
+++ aws-infrastructure/security-groups.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "my-security-group" {
+    name="webserver-sg"
+    vpc_id = aws_vpc.aws_static_website_hosting.id
+    description = "allow all"
+
+    ingress = {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_block = ["0.0.0.0/0"]
+    }
+    
+    egress = {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_block = ["0.0.0.0/0"]
+    }
+}
